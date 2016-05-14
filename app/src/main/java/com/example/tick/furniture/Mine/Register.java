1709226465c8119bfb45c9d65c8b73998dbe3def
Tick <334949356@qq.com>
2016-05-15 00:03:52 +0800

    增加了短信验证ui，逻辑未实现

--- app/src/main/java/com/example/tick/furniture/Mine/Register.java
+++ app/src/main/java/com/example/tick/furniture/Mine/Register.java
@@ -1,13 +1,23 @@
 package com.example.tick.furniture.Mine;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.Context;
 import android.os.Bundle;
+import android.text.Layout;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.example.tick.furniture.R;
+
+import cn.bmob.sms.BmobSMS;
+import cn.bmob.sms.exception.BmobException;
+import cn.bmob.sms.listener.RequestSMSCodeListener;
 
 /**
  * Created by Tick on 2016/5/14.
@@ -16,16 +26,22 @@ public class Register extends Activity implements View.OnClickListener{
     private ImageView iv_back;
     private CheckBox cb_pro;
     private Button bt_registernext;
+    private Context thisContext;//用于吐司显示窗口
+    private EditText et_phone;
+    private View dialogView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register);
+        thisContext = this;
         initView();
+        BmobSMS.initialize(this,"c6a4ff3935571d3c911e209822000610");
     }
     public void initView(){
         iv_back = (ImageView) findViewById(R.id.iv_phoneRegister_back);
         bt_registernext = (Button) findViewById(R.id.bt_registernext);
         cb_pro = (CheckBox) findViewById(R.id.cb_protocol);
+        et_phone = (EditText) findViewById(R.id.et_phone);
 
         cb_pro.setOnClickListener(this);
         iv_back.setOnClickListener(this);
@@ -45,7 +61,30 @@ public class Register extends Activity implements View.OnClickListener{
                     bt_registernext.setEnabled(true);
                 }
                 break;
+            case R.id.bt_registernext:
+                String num = et_phone.getText().toString().trim();
+                sendMessage(num);
+                break;
         }
     }
-
+    public void sendMessage(String num){
+  /*     BmobSMS.requestSMSCode(this, num, "Test1.0", new RequestSMSCodeListener() {
+           @Override
+           public void done(Integer integer, BmobException e) {
+               if(e==null){
+                   Toast.makeText(thisContext,"发送成功",Toast.LENGTH_SHORT).show();
+                   if(dialogView==null){
+                        dialogView = LayoutInflater.from(thisContext).inflate(R.layout.dialog_register,null);
+                   }
+                   new AlertDialog.Builder(thisContext).setView(dialogView).setMessage("").setPositiveButton("确定",null).show();
+               }
+           }
+       });
+       */
+        //测试
+        if(dialogView==null){
+            dialogView = LayoutInflater.from(thisContext).inflate(R.layout.dialog_register,null);
+        }
+        new AlertDialog.Builder(thisContext).setView(dialogView).setMessage("").setPositiveButton("确定",null).show();
+    }
 }
